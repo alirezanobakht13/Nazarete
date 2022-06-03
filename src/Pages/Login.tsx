fd616d78ad5137a8f75a5c87ee4dbd3ce260c7bb
alirezanobakht13 <a.nobakht13@gmail.com>
2022-06-03 21:03:52 +0430

    error message added to login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,10 +1,13 @@
 import { faKey, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { ReactDOM } from "react";
 import Footer from "../Components/Footer";
 
 export default function Login(props?: any) {
+
+    const [invalid,setInvalid] = useState(false)
+
     return (
         <div className="flex flex-col h-screen">
             <div className="flex justify-center items-center grow">
@@ -28,14 +31,19 @@ export default function Login(props?: any) {
                             <h6 className="w-1/2 text-xl font-normal leading-normal mt-0 mb-2 text-blueGray-800">
                                 Password:
                             </h6>
-                            <div className="relative flex w-full flex-wrap items-stretch mb-3">
+                            <div className="relative flex w-full flex-wrap items-stretch">
                                 <input type="text" placeholder="password" className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full pr-10" />
                                 <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 right-0 pr-3 py-3">
                                     <FontAwesomeIcon icon={faKey} />
                                 </span>
                             </div>
                         </div>
-                        <div className="flex w-full justify-end">
+                        <div className={"flex w-full justify-end mb-1 " + (!invalid? 'hidden':'')}>
+                        <small className="font-normal leading-normal text-red-500">
+                            Username password combination is wrong!
+                        </small>
+                        </div>
+                        <div className="flex w-full justify-end mt-2">
                             <button className="text-emerald-500 bg-transparent border border-solid border-emerald-500 hover:bg-emerald-500 hover:text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none ease-linear transition-all duration-150" type="button">
                                 Submit
                             </button>
